1d85c1a1a70fdbb5d9cdb9e5f78a2748bf6b3104
Pablo Buitrago <pablo@briefboards.com>
2023-12-27 18:41:31 -0300

    Added basic escrow.

--- src/evm/TransferTracker.sol
+++ src/evm/TransferTracker.sol
@@ -5,6 +5,9 @@ contract TransferTracker {
     mapping(address => uint256) private prevTransferIds;
     mapping(uint256 => TransferRecord) private transferRecords;
 
+    event FundsDeposited(uint256 amount, address escrowAddress);
+    event FundsWithdrawn(uint256 amount, address beneficiary);
+
     event TransferRequested(uint256 transferId, uint256 amount, address sender); 
     event TransferReceived(uint256 transferId, uint256 amount, address sender); 
 
@@ -20,6 +23,25 @@ contract TransferTracker {
         address sender; 
     }
 
+    address payable public escrow;
+
+    constructor() {
+        escrow = payable(msg.sender); // Set escrow to owner in constructor
+    }
+
+    function deposit() external payable {
+        require(msg.sender == escrow, "Only escrow can deposit");
+        emit FundsDeposited(msg.value, escrow);
+        // Handled directly by payable function
+    }
+
+    function withdraw(uint256 amount, address payable beneficiary) external {
+        require(msg.sender == escrow, "Only escrow can withdraw");
+        require(address(this).balance >= amount, "Insufficient contract balance");
+        beneficiary.transfer(amount);
+        emit FundsWithdrawn(amount, beneficiary);
+    }
+
     function requestTransfer(uint256 amount) external {
         uint256 transferId = _createTransferId(amount);
         transferRecords[transferId] = TransferRecord(amount, TransferStatus.Pending, msg.sender);
@@ -32,22 +54,21 @@ contract TransferTracker {
         require(record.status == TransferStatus.Pending, "Invalid transfer status");
 
         record.status = TransferStatus.Completed;
-        emit TransferReceived(transferId, record.amount, record.sender); // Emit event to indicate completion
+        emit TransferReceived(transferId, record.amount, record.sender);
     }
 
-    function receiveTransfer(uint256 transferId, address payable destination) external {
+    function receiveTransfer(uint256 transferId, address payable beneficiary) external {
         TransferRecord storage record = transferRecords[transferId];
         require(record.status == TransferStatus.Pending, "Invalid transfer status");
         require(record.amount > 0, "Invalid transfer amount");
         require(record.sender == msg.sender, "Unauthorized sender"); 
 
-        // Ensure destination address is valid
-        require(destination != address(0), "Invalid destination address");
+        // Ensure beneficiary address is valid
+        require(beneficiary != address(0), "Invalid beneficiary address");
 
-        // ToDo: Implement a separated Escrow contract.
         // Transfer funds from the contract
         require(address(this).balance >= record.amount, "Insufficient contract balance");
-        destination.transfer(record.amount);
+        beneficiary.transfer(record.amount);
 
         emit TransferReceived(transferId, record.amount, record.sender);
         record.status = TransferStatus.Completed;
@@ -55,7 +76,7 @@ contract TransferTracker {
 
     function _createTransferId(uint256 amount) internal returns (uint256) {
         require(msg.sender != address(0), "Invalid sender");
-        uint256 transferId = uint256(keccak256(abi.encodePacked(msg.sender, amount, prevTransferIds[msg.sender])));
+        uint256 transferId = uint256(keccak256(abi.encodePacked(msg.sender, amount, block.timestamp, prevTransferIds[msg.sender])));
         prevTransferIds[msg.sender] = transferId;
         return transferId;
     }
